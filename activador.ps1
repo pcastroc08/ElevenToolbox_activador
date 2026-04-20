70809e808144632f7df3cd2f43768297778cde9e
pcastroc08 <p.castro.c08@gmail.com>
2026-04-20 22:51:47 +0200

    Update activador.ps1

--- activador.ps1
+++ activador.ps1
@@ -1,11 +1,4 @@
-# URL del CMD original (sin modificar)
 $cmdUrl = "https://raw.githubusercontent.com/pcastroc08/ElevenToolbox_activador/main/HWID_Activation.cmd"
-
-# Ruta temporal donde se guardará
 $cmdPath = "$env:TEMP\HWID.cmd"
-
-# Descargar el CMD desde GitHub
 Invoke-WebRequest -Uri $cmdUrl -OutFile $cmdPath
-
-# Ejecutar el CMD como administrador
 Start-Process cmd.exe -ArgumentList "/c `"$cmdPath`"" -Verb RunAs
