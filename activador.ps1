f00126aec0b205615bbcb00b3f72d9e29b9df5dd
pcastroc08 <p.castro.c08@gmail.com>
2026-05-20 18:07:47 +0200

    Update activador.ps1

--- activador.ps1
+++ activador.ps1
@@ -1,6 +1,4 @@
-# $cmdUrl = "https://raw.githubusercontent.com/pcastroc08/ElevenToolbox_activador/main/HWID_Activation.cmd"
-
-$cmdUrl = "https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/master/MAS/Separate-Files-Version/Activators/HWID_Activation.cmd"
+$cmdUrl = "https://raw.githubusercontent.com/pcastroc08/ElevenToolbox_activador/main/HWID_Activation.cmd"
 $cmdPath = "$env:TEMP\HWID.cmd"
 Invoke-WebRequest -Uri $cmdUrl -OutFile $cmdPath
 Start-Process cmd.exe -ArgumentList "/c `"$cmdPath`"" -Verb RunAs
